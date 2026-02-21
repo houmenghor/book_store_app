334ad8bbcc6c3347916915121737478015e6c0dc
Hou <menghorhou@gmail.com>
2026-02-21 18:50:57 +0700

    change base url

--- lib/core/config/env.dart
+++ lib/core/config/env.dart
@@ -3,6 +3,6 @@
 
   static const String apiBaseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'http://10.0.2.2:8000/api',
+    defaultValue: 'https://bookstoreapi.sainnovationresearchlab.com/api',
   );
 }
